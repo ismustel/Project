79e0f43f3d3589220628dc7d11373dd66bb84477
elias <146341983+ismustel@users.noreply.github.com>
2023-10-06 22:13:57 +0700

    Add files via upload

---
+++ 2 10.py
@@ -0,0 +1,19 @@
+
+a = [-7, 5, 4, -3, -1.2, 5, 10]
+product_negative = 1
+summ_pos = 0
+max = a[0]
+b = [0]*len(a)
+
+for i in range(len(a)):
+    if a[i] > max:
+        max = a[i]
+    if 0 < a[i] < max:
+        summ_pos += a[i]
+    elif a[i] < 0:
+        product_negative *= a[i]
+    b[i] = a[len(a)-1-i]
+
+print(b) #revers array
+print(product_negative)
+print(summ_pos)

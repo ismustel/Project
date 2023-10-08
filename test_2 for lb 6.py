1ec97d927d63351d11a61803ae50a6aaafb0ec7f
elias <elias@ustisha.ru>
2023-10-08 23:19:42 +0700

    lb6

--- test_2 for lb 6.py
+++ test_2 for lb 6.py
@@ -1,23 +1,32 @@
-from random import randint
-
-m = int(input('Input number of lines: '))
-n = int(input('Input number of lines: '))
-a = []
-
-for i in range(m):
-    b = []
-    for j in range(n):
-        b.append(randint(0, 20))
-    a.append(b)
-# a = [[1,2,3,4],
-#      [6,2,6,1],
-#      [7,2,9,1],
-#      [1,4,5,6],
-#      [1,4,3,1]]
-print(a)
+# from random import randint
+#
+# m = int(input('Input number of lines: '))
+# n = int(input('Input number of lines: '))
+# a = []
+#
+# for i in range(m):
+#     b = []
+#     for j in range(n):
+#         b.append(randint(0, 20))
+#     a.append(b)
+a = [[1,2,3,4],
+     [6,2,6,1],
+     [7,2,9,1],
+     [1,4,0,6],
+     [1,4,3,1]]
 
 c = a[0][0]
 summ_1 = 1
+summ_3 = 1
+max_column = 0
+summ_zero_lines = 0
+print(a)
+
+for k in a:
+    for t in range(len(k)):
+        if k[t] == 0:
+            summ_zero_lines += 1
+print('Number of lines with zero', summ_zero_lines)
 
 for i in range(4):
     summ_2 = 0
@@ -34,4 +43,8 @@ for i in range(4):
             if summ_1 > summ_2:
                 summ_2 = summ_1
             summ_1 = 1
-    print(summ_2)
+
+    if summ_2 > summ_3:
+        summ_3 = summ_2
+        max_column +=1
+print('Columns:', max_column)
